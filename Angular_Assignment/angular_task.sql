51d0d050893bab16f66fd59cbb608dd69cb32ac4
P Krishna Dheeraj <63544142+krishnadheerajp@users.noreply.github.com>
2023-02-02 10:39:38 +0530

    uploading sql file

---
+++ Angular_Assignment/angular_task.sql
@@ -0,0 +1,8 @@
+-- create table Empdetails(empcode integer,name varchar(20),designation varchar(20),email varchar(25),location varchar(20));
+
+-- insert into Empdetails values
+-- (1,'aman','manager','am@gmail.com','mumbai'),
+-- (2,'raj','developer','rj@gmail.com','pune'),
+-- (3,'sanjay','lead','sj@gmail.com','bangalore');
+
+select * from Empdetails;
